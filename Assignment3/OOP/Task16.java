26a69cb3aeb4595f59fd7fb5d9bf1a7295d4d601
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:58:05 +0500

    Create Task16.java

---
+++ Assignment3/OOP/Task16.java
@@ -0,0 +1,25 @@
+package Assignment3.OOP;
+
+public class Task16 {
+    private String model;
+    private int year;
+    private String color;
+
+    public void initialize(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
