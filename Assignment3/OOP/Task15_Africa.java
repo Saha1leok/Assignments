ab69258ceaf42991e40ce02dc164d671343c5bb4
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:28:16 +0500

    Update Task15_Africa.java

--- Assignment3/OOP/Task15_Africa.java
+++ Assignment3/OOP/Task15_Africa.java
@@ -1,9 +1,9 @@
 package Assignment3.OOP;
 
-public class Africa {
+public class Task15_Africa {
     private final int area;
 
-    public Africa(int area) {
+    public Task15_Africa(int area) {
         this.area = area;
     }
 
