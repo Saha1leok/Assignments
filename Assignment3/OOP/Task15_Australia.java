ca5fd1e53326ddb29032bb19a887ae7aac4d08af
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:23:35 +0500

    Create Task15_Australia.java

---
+++ Assignment3/OOP/Task15_Australia.java
@@ -0,0 +1,13 @@
+package Assignment3.OOP_part2.Task15all;
+
+public class Task15_Australia {
+    private final int area;
+
+    public Task15_Australia(int area) {
+        this.area = area;
+    }
+
+    public int getArea() {
+        return area;
+    }
+}
