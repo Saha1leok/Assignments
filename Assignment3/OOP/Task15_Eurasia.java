b3d687b4cec7e3a4bf8371729ac61e48d9c3fcdf
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:24:56 +0500

    Create Task15_Eurasia.java

---
+++ Assignment3/OOP/Task15_Eurasia.java
@@ -0,0 +1,13 @@
+package Assignment3.OOP;
+
+public class Task15_Eurasia {
+    private final int area;
+
+    public Task15_Eurasia(int area) {
+        this.area = area;
+    }
+
+    public int getArea() {
+        return area;
+    }
+}
