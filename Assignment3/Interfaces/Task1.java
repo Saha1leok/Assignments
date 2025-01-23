71af9fca743a0b47bb9e9ba174cb29eed9b2bbd8
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 22:02:49 +0500

    Create Task1.java

---
+++ Assignment3/Interfaces/Task1.java
@@ -0,0 +1,12 @@
+package Assignment3.Interfaces;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+    public interface CanFly {
+        void Fly();
+
+        int FlightSpeed();
+    }
+}
