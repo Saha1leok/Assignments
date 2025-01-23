1b5e2529863242ccd74bac8e10e86314312a3a92
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:40:25 +0500

    Update and rename Task3_2.java to Skyscraper_Task3.java

--- Assignment3/OOP/Skyscraper_Task3.java
+++ Assignment3/OOP/Skyscraper_Task3.java
@@ -1,15 +1,15 @@
 package Assignment3.OOP;
 
-public class Skyscraper_task3 {
+public class Skyscraper_Task3 {
     private int floorsCount;
     private String developer;
 
-    public Skyscraper_task3() {
+    public Skyscraper_Task3() {
         this.floorsCount = 5;
         this.developer = "JavaRushDevelopment";
     }
 
-    public Skyscraper_task3(int floorsCount, String developer) {
+    public Skyscraper_Task3(int floorsCount, String developer) {
         this.floorsCount = floorsCount;
         this.developer = developer;
     }
