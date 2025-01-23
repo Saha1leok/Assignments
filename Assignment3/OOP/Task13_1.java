a58765552347e3ab9447919e1c5bb467d96faee3
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 20:57:34 +0500

    Create Task13_1.java

---
+++ Assignment3/OOP/Task13_1.java
@@ -0,0 +1,18 @@
+package Assignment3.OOP;
+
+public class Task13 {
+    public void eat(Apple apple) {
+        System.out.println("The apple has been eaten!");
+    }
+
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+
+        Hedgehog hedgehog = new Hedgehog();
+
+        hedgehog.eat(apple);
+    }
+
+    public static class Apple {
+    }
+}
