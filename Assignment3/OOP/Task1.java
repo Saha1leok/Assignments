065871cada793c32234ddd08d356807877e6f774
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:36:25 +0500

    Update and rename Task1_1.java to Task1.java

---
+++ Assignment3/OOP/Task1.java
@@ -0,0 +1,7 @@
+package Assignment3.OOP;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Task1_Skyscraper skyscraperTask1 = new Task1_Skyscraper();
+    }
+}
--- Assignment3/OOP/Task1_1.java
+++
@@ -1,7 +0,0 @@
-package Assignment3.OOP;
-
-public class Task1_1 {
-    public static void main(String[] args) {
-        Skyscraper_Task1 skyscraperTask1 = new Skyscraper_Task1();
-    }
-}
