9e611f98b44b301c7b1749406faf34cb41730328
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 20:36:50 +0500

    Create Task6_2.java

---
+++ Assignment3/OOP/Task6_2.java
@@ -0,0 +1,33 @@
+package Assignment3.OOP;
+
+public class Bugatti {
+        private String color = "BLACK";
+        private int year = 2020;
+        private String body = "Coupe";
+
+        public Bugatti() {
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public int getYear() {
+            return year;
+        }
+        public void setYear(int year) {
+            this.year = year;
+        }
+}
