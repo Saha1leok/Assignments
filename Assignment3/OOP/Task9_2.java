d991406365c91e610137e13ff048ee9bb0642ca0
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 20:44:35 +0500

    Create Task9_2.java

---
+++ Assignment3/OOP/Task9_2.java
@@ -0,0 +1,10 @@
+package Assignment3.OOP;
+
+public class Iphone {
+    public String model;
+    public String color;
+    public int price;
+
+    public Iphone(String x, String black, int i) {
+    }
+}
