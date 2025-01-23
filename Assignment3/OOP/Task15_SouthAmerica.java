fd5ad019de7b41b92cf347b4324b5fc20fbb2d56
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:27:04 +0500

    Create Task15_SouthAmerica.java

---
+++ Assignment3/OOP/Task15_SouthAmerica.java
@@ -0,0 +1,13 @@
+package Assignment3.OOP;
+
+public class Task15_SouthAmerica {
+    private final int area;
+
+    public Task15_SouthAmerica(int area) {
+        this.area = area;
+    }
+
+    public int getArea() {
+        return area;
+    }
+}
