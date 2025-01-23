7aeb48a5563d5dd89ed8bf53b494372068f5d329
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:37:07 +0500

    Update and rename Task2_1.java to Task2.java

---
+++ Assignment3/OOP/Task2.java
@@ -0,0 +1,9 @@
+package Assignment3.OOP;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Skyscraper_Task2 skyscraper= new Skyscraper_task2();
+        Skyscraper_Task2 skyscraperTower = new Skyscraper_task2(50);
+        Skyscraper_Task2 skyscraperSkyline = new Skyscraper_task2("JavaRushDevelopment");
+    }
+}
--- Assignment3/OOP/Task2_1.java
+++
@@ -1,9 +0,0 @@
-package Assignment3.OOP;
-
-public class Task2 {
-    public static void main(String[] args) {
-        Skyscraper_task2 skyscraper= new Skyscraper_task2();
-        Skyscraper_task2 skyscraperTower = new Skyscraper_task2(50);
-        Skyscraper_task2 skyscraperSkyline = new Skyscraper_task2("JavaRushDevelopment");
-    }
-}
