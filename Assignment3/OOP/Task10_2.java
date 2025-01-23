165a5973e8043d0844a428154b609e7f2194708d
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:50:13 +0500

    Update and rename Task10_2.java to Task10_Car.java

--- Assignment3/OOP/Task10_2.java
+++
@@ -1,6 +0,0 @@
-package Assignment3.OOP;
-
-public class Car {
-    public Car(String car, int year) {
-    }
-}
---
+++ Assignment3/OOP/Task10_Car.java
@@ -0,0 +1,6 @@
+package Assignment3.OOP;
+
+public class Task10_Car {
+    public Task10_Car(String car, int year) {
+    }
+}
