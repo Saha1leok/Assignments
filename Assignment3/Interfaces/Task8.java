b1cbf39ee0e3c34a84c89d483e3a546ef95ee237
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 22:17:55 +0500

    Create Task8.java

---
+++ Assignment3/Interfaces/Task8.java
@@ -0,0 +1,15 @@
+package Assignment3.Interfaces;
+
+public class Task8 {
+    public static void main(String[] args) throws Exception {
+
+
+
+    }
+    public interface Person {
+        boolean IsAlive();
+    }
+    public interface Presentable extends Person {
+
+    }
+}
