bbe8539e68bb00f4b72e133949f0d9e9c896faa4
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 22:27:21 +0500

    Create Person.java

---
+++ Assignment3/Interfaces/Task18/Person.java
@@ -0,0 +1,9 @@
+ackage Assignment3.Interfaces.Task18;
+
+public class Person {
+    public Person(String granddaughter, String granddaughter1) {
+    }
+
+    public void pull(Person first) {
+    }
+}
