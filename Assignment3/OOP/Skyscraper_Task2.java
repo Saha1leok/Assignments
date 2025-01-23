17ad5e17b76fa57e90177b551473816bcb7123ad
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:38:18 +0500

    Update and rename Task2_2.java to Skyscraper_Task2.java

--- Assignment3/OOP/Skyscraper_Task2.java
+++ Assignment3/OOP/Skyscraper_Task2.java
@@ -1,19 +1,19 @@
 package Assignment3.OOP;
 
-public class Skyscraper_task2 {
+public class Skyscraper_Task2 {
     public static final String SKYSCRAPER_WAS_BUILD = "Skyscraper is built.";
     public static final String SKYSCRAPER_WAS_BUILD_FLOORS_COUNT = "Skyscraper is built. The number of floors - ";
     public static final String SKYSCRAPER_WAS_BUILD_DEVELOPER = "Skyscraper is built. Developer - ";
 
-    public Skyscraper_task2() {
+    public Skyscraper_Task2() {
         System.out.println(SKYSCRAPER_WAS_BUILD);
     }
 
-    public Skyscraper_task2(int floors) {
+    public Skyscraper_Task2(int floors) {
         System.out.println(SKYSCRAPER_WAS_BUILD_FLOORS_COUNT + floors);
     }
 
-    public Skyscraper_task2(String developer) {
+    public Skyscraper_Task2(String developer) {
         System.out.println(SKYSCRAPER_WAS_BUILD_DEVELOPER + developer);
     }
 }
