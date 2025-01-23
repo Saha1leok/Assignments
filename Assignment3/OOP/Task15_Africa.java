648a4186a111ce0342ca58d1d9a558b976f54a73
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:20:54 +0500

    Create Task15_Africa.java

---
+++ Assignment3/OOP/Task15_Africa.java
@@ -0,0 +1,13 @@
+package Assignment3.OOP;
+
+public class Africa {
+    private final int area;
+
+    public Africa(int area) {
+        this.area = area;
+    }
+
+    public int getArea() {
+        return area;
+    }
+}
