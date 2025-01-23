00f9eb11528ea86c0ea45ab86803ca660950038f
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 21:55:42 +0500

    Update Task2.java

--- Assignment3/OOP/Task2.java
+++ Assignment3/OOP/Task2.java
@@ -2,8 +2,8 @@ package Assignment3.OOP;
 
 public class Task2 {
     public static void main(String[] args) {
-        Skyscraper_Task2 skyscraper= new Skyscraper_task2();
-        Skyscraper_Task2 skyscraperTower = new Skyscraper_task2(50);
-        Skyscraper_Task2 skyscraperSkyline = new Skyscraper_task2("JavaRushDevelopment");
+        Skyscraper_Task2 skyscraper= new Skyscraper_Task2();
+        Skyscraper_Task2 skyscraperTower = new Skyscraper_Task2(50);
+        Skyscraper_Task2 skyscraperSkyline = new Skyscraper_Task2("JavaRushDevelopment");
     }
 }
