d3d2d628a119ac75e9ff67126b634a87e16acda9
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 22:20:50 +0500

    Create Task11.java

---
+++ Assignment3/Interfaces/Task11.java
@@ -0,0 +1,64 @@
+package Assignment3.Interfaces;
+
+public class Task11 {
+    public static void main(String[] args) {
+    }
+
+    //can move
+    public interface Movable {
+        void move();
+    }
+
+    //can be eaten
+    public interface Edible {
+        void beEaten();
+    }
+
+    // might eat someone
+    public interface Eat {
+        void eat();
+    }
+
+    class Dog implements Movable, Eat {
+
+        @Override
+        public void eat() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+    }
+
+    class Cat implements Movable, Edible, Eat {
+
+        @Override
+        public void eat() {
+
+        }
+
+        @Override
+        public void beEaten() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+    }
+
+    class Mouse implements Movable, Edible {
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void beEaten() {
+
+        }
+    }
+}
