08e81fcec3629d39f1f92fed25c3d0825efa5bf3
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 20:32:07 +0500

    Create Task3_2.java

---
+++ Assignment3/OOP/Task3_2.java
@@ -0,0 +1,17 @@
+package Assignment3.OOP;
+
+public class Skyscraper_task3 {
+    private int floorsCount;
+    private String developer;
+
+    public Skyscraper_task3() {
+        this.floorsCount = 5;
+        this.developer = "JavaRushDevelopment";
+    }
+
+    public Skyscraper_task3(int floorsCount, String developer) {
+        this.floorsCount = floorsCount;
+        this.developer = developer;
+    }
+
+}
