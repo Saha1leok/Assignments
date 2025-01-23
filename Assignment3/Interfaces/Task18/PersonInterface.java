36d4ab20b59f6a2705fa91e9cc6bbf6a3734f268
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 22:28:57 +0500

    Create PersonInterface.java

---
+++ Assignment3/Interfaces/Task18/PersonInterface.java
@@ -0,0 +1,33 @@
+package Assignment3.Interfaces.Task18;
+
+public abstract class PersonInterface implements RepkaItem {
+    public class Person implements RepkaItem {
+        private String name;
+        private String namePadezh;
+
+        public Person(String name, String namePadezh) {
+            this.name = name;
+            this.namePadezh = namePadezh;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getNamePadezh() {
+            return namePadezh;
+        }
+
+        public void setNamePadezh(String namePadezh) {
+            this.namePadezh = namePadezh;
+        }
+
+        public void pull(Person person) {
+            System.out.println(this.name + " for " + person.getNamePadezh());
+        }
+    }
+}
