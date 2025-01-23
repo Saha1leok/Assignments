816121041941a31b8fd8dfb208f33cbb63b77b89
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-01-23 20:30:06 +0500

    Create Task3_1.java

---
+++ Assignment3/OOP/Task3_1.java
@@ -0,0 +1,9 @@
+package Assignment3.OOP;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Skyscraper_task3 skyscraper = new Skyscraper_task3();
+        Skyscraper_task3 skyscraperUnknown = new Skyscraper_task3(50, "Unknown");
+    }
+}
