1f2c9850be4f6a38ea5d563eec25c8d899c107e4
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-02-02 19:05:56 +0500

    Update Airport.java

--- Assignment4/Airport.java
+++ Assignment4/Airport.java
@@ -1,36 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
 class Airport {
-    private String name; // Имя аэропорта
-    private List<Aircraft> aircraftVehicles; // Список самолетов
+    private String name;
+    private List<Aircraft> aircraftVehicles; 
 
-    // Конструктор для создания аэропорта
+    
     public Airport(String name) {
         this.name = name;
         this.aircraftVehicles = new ArrayList<>();
     }
 
-    // Метод для получения имени аэропорта
+    
     public String getName() {
         return name;
     }
 
-    // Метод для получения списка самолетов
+   
     public List<Aircraft> getAircraftVehicles() {
         return aircraftVehicles;
     }
 
-    // Метод для добавления самолета в аэропорт
+   
     public void addAircraft(Aircraft aircraft) {
         aircraftVehicles.add(aircraft);
     }
 
-    // Метод для удаления самолета из аэропорта
+  
     public void removeAircraft(Aircraft aircraft) {
         aircraftVehicles.remove(aircraft);
     }
 
-    // Метод для поиска пассажира по названию самолета и номеру места
+   
     public Passenger searchPassenger(String aircraftName, int seatNumber) {
         for (Aircraft aircraft : aircraftVehicles) {
             if (aircraft.getName().equals(aircraftName)) {
