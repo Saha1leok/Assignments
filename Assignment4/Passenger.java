df0e9da322a5ead9c8676e071c1a794e942c525c
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-02-02 19:04:22 +0500

    Update Passenger.java

--- Assignment4/Passenger.java
+++ Assignment4/Passenger.java
@@ -1,30 +1,30 @@
 class Passenger {
-    private String name; // Имя пассажира
-    private int seatNumber; // Номер места
-    private Aircraft aircraft; // Самолет, к которому относится пассажир
+    private String name; 
+    private int seatNumber; 
+    private Aircraft aircraft; 
 
-    // Конструктор для создания пассажира
+   
     public Passenger(String name, int seatNumber) {
         this.name = name;
         this.seatNumber = seatNumber;
     }
 
-    // Метод для получения имени пассажира
+   
     public String getName() {
         return name;
     }
 
-    // Метод для получения номера места
+   
     public int getSeatNumber() {
         return seatNumber;
     }
 
-    // Метод для получения самолета
+    
     public Aircraft getAircraft() {
         return aircraft;
     }
 
-    // Метод для установки самолета
+    
     public void setAircraft(Aircraft aircraft) {
         this.aircraft = aircraft;
     }
