e4286b58664ffe2fb4d043c45283f4b4c5611cb5
Saha1leok <zhasqayrat.aiiau.my@gmail.com>
2025-02-10 23:19:58 +0500

    Update Passenger.java

--- Assignment4/Passenger.java
+++ Assignment4/Passenger.java
@@ -1,30 +1,25 @@
 class Passenger {
-    private String name; 
-    private int seatNumber; 
-    private Aircraft aircraft; 
+    private String name;
+    private int seatNumber;
+    private Aircraft aircraft;
 
-   
     public Passenger(String name, int seatNumber) {
         this.name = name;
         this.seatNumber = seatNumber;
     }
 
-   
     public String getName() {
         return name;
     }
 
-   
     public int getSeatNumber() {
         return seatNumber;
     }
 
-    
     public Aircraft getAircraft() {
         return aircraft;
     }
 
-    
     public void setAircraft(Aircraft aircraft) {
         this.aircraft = aircraft;
     }
